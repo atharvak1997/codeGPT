28440b59a70dc08ece536595be5393127c93bd89
Atharva Kukade <atharva.kukade@icloud.com>
2023-10-21 17:29:47 -0700

    Node module error

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
     // Fetch data from server
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codegpt-3w0l.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
