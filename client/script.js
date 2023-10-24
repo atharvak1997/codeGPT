4047aecb0f4619095fd41ea20034c534507527c3
Atharva Kukade <atharva.kukade@icloud.com>
2023-10-23 18:06:08 -0700

    Imports removed

--- client/script.js
+++ client/script.js
@@ -1,8 +1,6 @@
 import bot from './assets/bot.svg';
 import user from "./assets/user.svg";
-import { useCompletion } from "ai/react";
-import React from 'react';
-import ReactDOM from 'react-dom';
+
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container')
